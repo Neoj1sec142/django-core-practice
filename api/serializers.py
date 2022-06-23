c3a56df9b334f9aea0c74aaf787412b0d9d530a6
Mark Harmon <neothej1sec@gmail.com>
2022-06-23 16:15:47 -0500

    finished up basic serializers

--- api/serializers.py
+++ api/serializers.py
@@ -4,24 +4,27 @@ from .models import Language, Post, Comment
 class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         models = Language
-        fields = ('__all__')
+        fields = '__all__'
+    
 
 class PostSerializer(serializers.ModelSerializer):
-    language = serializers.HyperlinkedRelatedField(
-        view_name='post_detail'
-        many=True
+    language = LanguageSerializer(
+        view_name='post_detail',
+        many=True,
         read_only=True
     )
     class Meta:
         model = Post
-        fields = ('__all__')
+        fields = '__all__'
+        extra_fields = ('language')
 
 class CommentSerializer(serializers.ModelSerializer):
-    post = serializers.HyperlinkedRelatedField(
-        view_name='comment_detail'
-        many=True
+    post = PostSerializer(
+        view_name='comment_detail',
+        many=True,
         read_only=True 
     )
     class Meta: 
         model = Comment
-        fields = ('__all__')
+        fields = '__all__'
+        extra_fields = ('post')
