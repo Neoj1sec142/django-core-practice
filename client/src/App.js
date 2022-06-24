45859c172dd06e9e5e76dd4b17a73e4f8976a6fd
Mark Harmon <neothej1sec@gmail.com>
2022-06-24 02:33:26 -0500

    set up basic login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import React, {useEffect, useState} from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LoginContext from './LoginContext'
+import Client from './services/Axios'
 import './styles/App.css';
 
-function App() {
+const App = () => {
+  const [loginStatus, setLoginStatus] = useState(false)
+  const [user, setUser] = useState({})
+  useEffect(() => {
+    const user_id = localStorage.getItem('user_id')
+    const username = localStorage.getItem('username')
+    setUser(user_id, username)
+    if(user_id && username){
+      if(loginTest(username)) setLoginStatus(true)
+    }else setLoginStatus(false)
+  }, [loginStatus])
+
+  const loginTest = async (username) => {
+    await Client.get(`users/${username}`)
+    .then(res => {
+      if (res.status === 200){
+        setLoginStatus(true)
+        return true
+      }else{
+        setLoginStatus(false)
+        return false
+      }
+    })
+    .catch(err => {
+      setLoginStatus(false)
+      console.log(err, "ERROR HERE")
+    })
+  }
   return (
     <div className="App">
       <LoginContext.Provider value={{loginStatus, setLoginStatus, user, setUser}}>
